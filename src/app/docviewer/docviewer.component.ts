f29a5a65e21af638971c5f93830b7eddd5c3034c
MSurendarReddy <m.soori2806@gmail.com>
2020-05-27 00:00:02 +0530

    doc veiwer

--- src/app/docviewer/docviewer.component.ts
+++ src/app/docviewer/docviewer.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./docviewer.component.css']
 })
 export class DocviewerComponent implements OnInit {
-
+  url = `${getbaseUrl()}/assets/ppt1.ppt`
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
 }
+
+export const getbaseUrl = (): string => {
+  alert("test")
+  const pathArray = window.location.href.split('/');
+  const protocol = pathArray[0];
+  const host = pathArray[2];
+  return protocol + '//' + host;
+};
--- src/app/excel/excel.component.ts
+++ src/app/excel/excel.component.ts
@@ -14,20 +14,19 @@ export class ExcelComponent implements OnInit {
   }
   ngAfterViewInit() {
     jexcel(document.getElementById("spreadsheet"), {
-      data: [[]],
-      // columns: [
-      //   { type: "dropdown", width: "100px", source: ["Y", "N"] },
-      //   { type: "color", width: "100px", render: "square" }
-      // ],
-      columns: [
-        { type: 'text', title:'Car', width:120 },
-        { type: 'dropdown', title:'Make', width:200, source:[ "Alfa Romeo", "Audi", "Bmw" ] },
-        { type: 'calendar', title:'Available', width:200 },
-        { type: 'image', title:'Photo', width:120 },
-        { type: 'checkbox', title:'Stock', width:80 },
-        { type: 'numeric', title:'Price', width:100, mask:'$ #.##,00', decimal:',' },
-        { type: 'color', width:100, render:'square', }
-     ],
+      data: [
+        ['Jazz', 'Honda', '2019-02-12', '', true, '$ 2.000,00', '#777700'],
+        ['Civic', 'Honda', '2018-07-11', '', true, '$ 4.000,01', '#007777']
+      ],
+     columns: [
+        { type: 'text', title: 'Car', width: 120 },
+        { type: 'dropdown', title: 'Make', width: 200, source: ["Alfa Romeo", "Audi", "Bmw","Honda"] },
+        { type: 'calendar', title: 'Available', width: 200 },
+        { type: 'image', title: 'Photo', width: 120 },
+        { type: 'checkbox', title: 'Stock', width: 80 },
+        { type: 'numeric', title: 'Price', width: 100, mask: '$ #.##,00', decimal: ',' },
+        { type: 'color', width: 100, render: 'square', }
+      ],
 
       minDimensions: [10, 10]
     });
