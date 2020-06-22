895631292abdfe8d0a32a263cd2e73c088fd8b76
Surendar Reddy <45162280+MSurendarReddy@users.noreply.github.com>
2020-06-22 19:51:28 +0530

    t

--- src/temp.ts
+++ src/temp.ts
@@ -1,110 +1,183 @@
-import { Component, OnInit,ViewChild,EventEmitter, Output, Input } from '@angular/core';
-import { ImageViewerComponent } from 'ng2-image-viewer';
-
+import { Component, OnInit, Input, ViewChild, ViewContainerRef, ComponentFactoryResolver, Output, EventEmitter } from '@angular/core';
+import * as Tiff from 'tiff.js'
 @Component({
-  selector: 'lib-image',
-  templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+  selector: 'lib-common-doc-viewer',
+  templateUrl: './common-doc-viewer.component.html',
+  styleUrls: ['./common-doc-viewer.component.scss']
 })
-export class ImageComponent implements OnInit {
+export class CommonDocViewerComponent implements OnInit {
 
-  @ViewChild(ImageViewerComponent, { static: false }) private imageComponent: ImageViewerComponent;
-  
-  @Output('print') print : EventEmitter<any> = new EventEmitter();
-  @Output('download') download : EventEmitter<any> = new EventEmitter();
-  @Input() public images: any[];
+  @Input() public fileSrc: string;
+  @Input() public fileType: string;
 
-  page = 1;
-  zoom: number;
-  rotation: number;
+  @Output('print') print: EventEmitter<any> = new EventEmitter();
+  @Output('download') download: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  @ViewChild('componentPlaceholder', { read: ViewContainerRef, static: true }) public componentPlaceholder: ViewContainerRef;
+
+  public extension: string;
+  public base64Data: string
+  public images: any[];
+
+  public pdfCompRef: any
+  public imageCompRef: any
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
 
   ngOnInit(): void {
-    this.zoom = 1;
-    this.rotation = 0;
+  
   }
-
-  parentFun(type) {
-    // alert(type.operation);
-    // console.log("type", type.operation)
-    switch (type.operation) {
-      case 'zoomIn':
-        console.log("zoom in")
-        this.imageComponent.zoomIn();
-        // this.zoomIn();
-        break;
-      case 'zoomOut':
-        // this.zoomOut();
-        this.imageComponent.zoomOut();
-        break;
-      case 'roteteRight':
-        this.imageComponent.rotacionarDireita();
-        break;
-      case 'roteteLeft':
-        this.imageComponent.rotacionarEsquerda();
-        break;
-      case 'resetimageZoom':
-        this.imageComponent.resetarZoom();
-        break;
-      case 'downloadFile':
-        this.downloadFile();
-        break;
-      case 'searchInPDF':
-        // this.searchInPDF(type.newQuery);
-        break;
-      case 'incrementPage':
-        // this.incrementPage(type.amount);
-        break;
-      case 'fullScreen':
-        this.imageComponent.mostrarFullscreen();
-        break;
-      case 'printFile':
-        this.printFile();
-        break;
-      default:
-        alert("invalid operation");
-        alert(type.operation)
-
+  ngAfterViewInit() {
+    if (this.fileType == "pdf") {
+      this.enrichPdfComponent()
+    } else {
+      this.enrichImageComponent()
     }
   }
 
-  downloadFile() {
-
-    var link = document.createElement('a');
-    link.href = '' + this.images[0];
-    link.download = 'Download.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-
-    this.download.emit('download')
+  enrichImageComponent() {
+    if (this.isURlImage()) {
+      console.log("------------isURlImage------------");
+      this.extension = this.isFileExtenstion();
+      if (this.extension == "TIF" || this.extension == "tiff") {
+        this.convertTiffImageToPng(this.fileSrc);
+        console.log("this.fileSrc base64Data", this.fileSrc);
+        setTimeout(() => {
+          this.loadImageComponent();
+        }, 1000);
+      } else {
+        this.loadImageComponent()
+      }
+    } else {
+      this.loadImageComponent()
+    }
   }
-  printFile() {
-    window.print();
-    this.print.emit('print file');
+
+  loadImageComponent() {
+    this.componentPlaceholder.clear();
+    import('../image/image.module').then(({ ImageModule }) => {
+      const MyComponent = ImageModule.getMyComponent();
+      const factory = this.componentFactoryResolver.resolveComponentFactory(MyComponent);
+      this.imageCompRef = this.componentPlaceholder.createComponent(factory);
+      console.log("this.fileSrc in loadImageComponent", this.fileSrc)
+      if (this.extension == "TIF" || this.extension == "tiff") {
+        this.images = [this.base64Data.replace(`data:image/png;base64,`, '')]
+      } else {
+        this.images = [this.fileSrc.replace(`data:image/png;base64,`, '')]
+      }
+
+      this.imageCompRef.instance.images = this.images;
+
+      this.imageCompRef.instance.print.subscribe((e) => {
+        this.printCommon(e);
+      });
+      this.imageCompRef.instance.download.subscribe((e) => {
+        this.downloadCommon(e);
+      });
+
+    });
+
+  }
+ 
+
+  enrichPdfComponent() {
+    this.loadPdfComponent()
+  }
+
+  loadPdfComponent() {
+    this.componentPlaceholder.clear();
+    import('../pdf/pdf.module').then(({ PdfModule }) => {
+
+      const MyComponent = PdfModule.getMyComponent();
+      const factory = this.componentFactoryResolver.resolveComponentFactory(MyComponent);
+      this.pdfCompRef = this.componentPlaceholder.createComponent(factory);
+      this.pdfCompRef.instance.pdfSrc = this.fileSrc
+      // console.log('pdfCompRef', this.pdfCompRef)
+
+    });
+    // import('../pdf/pdf.module').then(({ PdfModule }) => {
+     
+    // });
+    // import('../pdf/pdf.component').then(({ PdfComponent }) => {
+
+    //   // const MyComponent = PdfModule.getMyComponent();
+    //   const factory = this.componentFactoryResolver.resolveComponentFactory(PdfComponent);
+    //   this.pdfCompRef = this.componentPlaceholder.createComponent(factory);
+    //   this.pdfCompRef.instance.pdfSrc = this.fileSrc
+    //   // console.log('pdfCompRef', this.pdfCompRef)
+
+    // });
+  }
+
+
+  isURlImage() {
+    return this.fileSrc.match(new RegExp(/^(https|http|www\.)/g));
+  }
+  isFileExtenstion() {
+    return this.fileSrc.match(new RegExp('[^.]+$'))[0];
+  }
+  convertTiffImageToPng(filename) {
+    // var base64DataTemp: string
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'arraybuffer';
+    xhr.open('GET', filename);
+    // xhr.addEventListener('loadend', this.handleEvent);
+    xhr.onload = (e) => {
+      var tiff = new Tiff({ buffer: xhr.response });
+      var width = tiff.width();
+      var height = tiff.height();
+      var canvas = tiff.toCanvas();
+      if (canvas) {
+        canvas.setAttribute('style', 'width:' + (width * 0.3) +
+          'px; height: ' + (height * 0.3) + 'px ;display:none');
+        var elem = document.createElement("div");
+        document.body.append(elem);
+        document.body.append(canvas);
+        this.base64Data = canvas.toDataURL("image/png");
+        console.log("====", this.base64Data);
+        // this.base64Data = base64DataTemp
+      }
+    };
+    xhr.send();
+  }
+
+  ngOnDestroy() {
+    if (this.pdfCompRef) {
+      this.pdfCompRef.destroy();
+    }
+    if (this.imageCompRef) {
+      this.imageCompRef.destroy();
+    }
+  }
+
+  printCommon(e) {
+    console.log("--------------printCommon---------------------", e)
+    this.print.emit(e)
+  }
+  downloadCommon(e) {
+    console.log("--------------downloadCommon---------------------", e)
+    this.download.emit(e)
   }
 
 }
 
 
-import { NgModule } from '@angular/core';
+
+import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ImageComponent } from './image.component';
-import { ImageViewerModule } from 'ng2-image-viewer';
-import { HeaderModule } from '../header/header.module';
+import { PdfComponent } from '../pdf/pdf.component';
+import { ImageComponent } from '../image/image.component';
 
 @NgModule({
-  declarations: [ImageComponent],
+  declarations: [],
   imports: [
-    CommonModule,
-    ImageViewerModule,
-    HeaderModule
-  ]
-  // entryComponents: [ImageComponent]
+    CommonModule
+  ], 
+  entryComponents: [PdfComponent,ImageComponent]
+  // exports:[HeaderModule,HeaderComponent]
 })
-export class ImageModule {
-  static getMyComponent() {
-    return ImageComponent
-  }
+export class CommonDocViewerModule { 
+
+ 
+
 }
